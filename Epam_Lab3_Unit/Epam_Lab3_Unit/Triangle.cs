34b27e1073e54d4f1b14ca59fbdcc5ad35536889
Myrku <33633493+Myrku@users.noreply.github.com>
2019-09-29 17:44:35 +0300

    add Epam_Lab3_UnitTests

---
+++ Epam_Lab3_Unit/Epam_Lab3_Unit/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_Lab3_Unit
+{
+    public class Triangle
+    {
+        public bool Triangle_Check(float a, float b, float c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a + b <= c || a + c <= b || c + b <= a)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
