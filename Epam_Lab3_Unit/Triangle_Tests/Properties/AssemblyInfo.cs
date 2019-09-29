17c39b78f0e96f3b8ff2dc4ebf472a37b0aa62a3
Myrku <33633493+Myrku@users.noreply.github.com>
2019-09-29 17:46:04 +0300

    add Epam_Lab3_UnitTests

---
+++ Epam_Lab3_Unit/Triangle_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Triangle_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triangle_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4793b0c8-e475-4103-bfca-8e23610d6c9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Epam_Lab3_Unit/Triangle_Tests/UnitTest1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Epam_Lab3_Unit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle_Tests
+{
+    [TestClass]
+    public class Triangle_Tests
+    {
+        [TestMethod]
+        public void A_Sidi_Is_Negative()
+        {
+            float a = -4.5f;
+            float b = 10;
+            float c = 3;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void B_Sidi_Is_Negative()
+        {
+            float a = 4;
+            float b = -10;
+            float c = 3;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void C_Sidi_Is_Negative()
+        {
+            float a = 4;
+            float b = 10;
+            float c = -3;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void A_Plus_B_More_C()
+        {
+            float a = 4;
+            float b = 10;
+            float c = 13;
+            bool expected = true;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void A_Plus_C_More_B()
+        {
+            float a = 8;
+            float b = 15;
+            float c = 13;
+            bool expected = true;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void C_Plus_B_More_A()
+        {
+            float a = 8;
+            float b = 15;
+            float c = 13;
+            bool expected = true;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void A_Plus_B_Less_C()
+        {
+            float a = 8;
+            float b = 15;
+            float c = 33;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void A_Plus_С_Less_B()
+        {
+            float a = 8;
+            float b = 15;
+            float c = 4;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void C_Plus_B_Less_A()
+        {
+            float a = 8;
+            float b = 1;
+            float c = 4;
+            bool expected = false;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void A_Equal_B_Equal_C()
+        {
+            float a = 5;
+            float b = 5;
+            float c = 5;
+            bool expected = true;
+
+            Triangle ABC = new Triangle();
+            bool actual = ABC.Triangle_Check(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+}
